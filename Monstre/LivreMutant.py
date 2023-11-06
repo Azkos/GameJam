0ddccfc55383ab5561cf255c72240c2e7d87d5d8
théo <tetebesset23@yahoo.com>
2023-11-06 16:10:50 +0100

    croquiq classe montsre alexis doit faire

--- Monstre/LivreMutant.py
+++ Monstre/LivreMutant.py
@@ -3,9 +3,8 @@ import pygame
 
 class LivreMutant:
     # Un personnage est défini par de l'attaque, de la vie, une competence, un sprite et un genre
-    def __init__(self, attaque, vie, competence, image_path):
+    def __init__(self, attaque, vie, image_path):
         self.attaque_base = attaque
         self.vie_base = vie
-        self.competence_base = competence
         self.image = pygame.image.load(image_path)
         self.rect = self.image.get_rect()
