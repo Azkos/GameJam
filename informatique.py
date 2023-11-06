131809d262caf97c63b36ac4032538f5a01b8920
rebeloa <rebelo.alexis@gmail.com>
2023-11-06 11:21:34 +0100

    Création de la classe informatique.py

---
+++ informatique.py
@@ -0,0 +1,10 @@
+# Création de la classe "informatique"
+
+import pygame
+
+class Informatique:
+
+    # Un personnage de type informatique possède une attaque moyenne et une vie moyenne
+    def __init__(self, attaque, vie):
+        self.attaque_base = attaque
+        self.vie_base = vie
