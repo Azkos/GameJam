892ffa21251765df5164fbb1a4787abe63601b3b
rebeloa <rebelo.alexis@gmail.com>
2023-11-06 15:33:33 +0100

    Ajout de toutes les classes nécessaires

---
+++ Personnage/Gea.py
@@ -0,0 +1,6 @@
+import Personne
+
+
+class Gea(Personne):
+    def __init__(self):
+        super().__init__(attaque=6, vie=80, competence="Réduction", sprite="sprite/perso-princ.png")
--- Personnage/Juridique.py
+++ Personnage/Juridique.py
@@ -2,5 +2,5 @@ import Personne
 
 
 class Juridique(Personne):
-    def __init__(self, genre):
-        super().__init__(attaque=6, vie=120, competence="Sommeil", sprite="sprite/perso-princ.png")
+    def __init__(self):
+        super().__init__(attaque=6, vie=120, competence="Coupable", sprite="sprite/perso-princ.png")
