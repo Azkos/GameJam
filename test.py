a84de8914f79017b4b61994dd0f3a29128c97d83
théo <tetebesset23@yahoo.com>
2023-11-06 14:35:28 +0100

    test classe

--- test.py
+++ test.py
@@ -17,7 +17,7 @@ noir = (0, 0, 0)
 font = pygame.font.Font(None, 36)
 
 # Chargez l'image d'arrière-plan
-fond = pygame.image.load("image/v_iut2-rentree-2023_1696500078894-jpg (2)_120x80.png")  # Remplacez "background.jpg" par le chemin de votre image d'arrière-plan
+fond = pygame.image.load("image/v_iut2-rentree-2023_1696500078894-jpg (2)_120x80.png")  # Remplacez par le chemin de votre image d'arrière-plan
 fond = pygame.transform.scale(fond, (largeur, hauteur))
 
 # Texte de l'écran de titre
@@ -25,10 +25,27 @@ titre_texte = font.render("Mon Jeu", True, blanc)
 jouer_texte = font.render("Appuyez sur une touche pour jouer", True, blanc)
 
 # Musique de l'écran de titre
-pygame.mixer_music.load('Musique/menu.mp3')
+pygame.mixer.music.load('Musique/menu.mp3')
 pygame.mixer.music.play(-1)
 
-# Boucle principale
+# Chargez votre propre image de sprite
+votre_sprite = pygame.image.load("sprite/perso-princ.png")  # Remplacez par le chemin de votre image de sprite
+votre_sprite = pygame.transform.scale(votre_sprite, (50, 50))  # Redimensionnez le sprite si nécessaire
+
+# Créez un objet sprite pour votre sprite personnalisé
+class MonSprite(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = votre_sprite
+        self.rect = self.image.get_rect()
+        self.rect.center = (largeur // 2, hauteur // 2)  # Position initiale de votre sprite
+
+# Créez un groupe de sprites
+sprites = pygame.sprite.Group()
+mon_sprite = MonSprite()
+sprites.add(mon_sprite)
+
+# Boucle principale pour l'écran de titre
 en_jeu = False
 while not en_jeu:
     for evenement in pygame.event.get():
@@ -51,8 +68,6 @@ while not en_jeu:
 
     pygame.display.flip()
 
-# Votre code de jeu commencera ici une fois que l'utilisateur appuie sur une touche
-
 # Boucle de jeu
 en_jeu = True
 while en_jeu:
@@ -62,10 +77,23 @@ while en_jeu:
 
     # Logique de jeu et affichage ici
 
+    # Mise à jour de la position de votre sprite en fonction des entrées de l'utilisateur
+    touches = pygame.key.get_pressed()
+    vitesse = 1
+    if touches[pygame.K_LEFT]:
+        mon_sprite.rect.x -= vitesse
+    if touches[pygame.K_RIGHT]:
+        mon_sprite.rect.x += vitesse
+    if touches[pygame.K_UP]:
+        mon_sprite.rect.y -= vitesse
+    if touches[pygame.K_DOWN]:
+        mon_sprite.rect.y += vitesse
+
     # Afficher l'arrière-plan du jeu
     fenetre.blit(fond, (0, 0))
 
-    # Dessiner le contenu de votre jeu par-dessus l'arrière-plan
+    # Afficher votre sprite
+    sprites.draw(fenetre)
 
     pygame.display.flip()
 
