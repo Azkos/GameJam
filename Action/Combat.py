8e485d328344060021f3c4f04dabbea8a5c1bef5
Yvain Dalban <yvain.dalban@gmail.com>
2023-11-07 09:43:28 +0100

    changement Combat et ajouts indicateurs pour le combat

--- Action/Combat.py
+++ Action/Combat.py
@@ -1,3 +1,5 @@
+import random
+import pygame
 
 
 class Combat:
@@ -5,13 +7,22 @@ class Combat:
     diff = {1 : 60, 2 : 90, 3 : 120}
 
 
-    def __init__(self, difficulte, personne, ennemi):
+    def __init__(self, difficulte, personne, ennemi, fenetre):
         self.difficulte = difficulte
         self.personne = personne
         self.ennemi = ennemi
+        self.nbEsquive = 0
+        self.fenetre = fenetre
+
+²
 
 
     def tour_de_combat(self):
+        action = random.randrange(1)
+        if self.nbEsquive == 2 or action == 1 :
+            self.nbEsquive = 0
+        else :
+            self.nbEsquive += 1
 
     def est_fini(self):
         #test la fin du combat
