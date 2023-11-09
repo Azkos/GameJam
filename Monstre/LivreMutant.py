6ed2506342e174e9be7ead6897017a4979308f75
Yvain Dalban <yvain.dalban@gmail.com>
2023-11-09 17:50:09 +0100

    suppression trucs inutiles

--- Monstre/LivreMutant.py
+++
@@ -1,6 +0,0 @@
-from Monstre import Ennemi
-
-
-class LivreMutant(Ennemi):
-    def __init__(self):
-        super().__init__(difficulte=1, image_path="sprit/livre.png")
--- Monstre/ProfZombie.py
+++
@@ -1,6 +0,0 @@
-from Monstre.Ennemi import Ennemi
-
-
-class ProfZombie(Ennemi):
-    def __init__(self):
-        super().__init__(difficulte=2, image_path="sprite/Prof-Zombie.png")
