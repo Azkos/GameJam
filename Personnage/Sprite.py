3eaa7e473a1562ad48dd1c3515e15a8a4f46ca92
théo <tetebesset23@yahoo.com>
2023-11-09 09:47:10 +0100

    nouveau déplacement

--- Personnage/Sprite.py
+++ Personnage/Sprite.py
@@ -5,9 +5,9 @@ hauteur = 800
 sprite_walk1 = pygame.image.load("sprite/chara-walk1.png")
 sprite_walk2 = pygame.image.load("sprite/chara-walk2.png")
 
+# Réduisez la taille des images de marche
 sprite_walk1 = pygame.transform.scale(sprite_walk1, (sprite_walk1.get_width() // 5, sprite_walk1.get_height() // 5))
 sprite_walk2 = pygame.transform.scale(sprite_walk2, (sprite_walk2.get_width() // 2, sprite_walk2.get_height() // 2))
-
 
 class Sprite(pygame.sprite.Sprite):
     def __init__(self):
@@ -17,40 +17,45 @@ class Sprite(pygame.sprite.Sprite):
         self.rect.center = (largeur // 2, hauteur // 3)  # Position initiale du sprite
         self.facing_left = False
         self.last_pos = (self.rect.x, self.rect.y)
+        # Créer une hitbox aux pieds du sprite
+        self.hitbox = self.rect.inflate(0, -self.rect.height * 0.8)  # Réduire la hitbox en hauteur
+        self.hitbox.bottom = self.rect.bottom  # Aligner le bas de la hitbox avec le bas du sprite
 
     def deplacement(self, vitesse):
         touches = pygame.key.get_pressed()
         direction = [0, 0]
 
         if touches[pygame.K_LEFT]:
-            if not self.facing_left:  # Si le sprite regarde vers la droite, inverser l'image une fois
+            if not self.facing_left:
                 self.image = pygame.transform.flip(self.image, True, False)
                 self.facing_left = True
-            direction[0] = -vitesse  # Déplacer le sprite vers la gauche
+            direction[0] = -vitesse
         elif touches[pygame.K_RIGHT]:
-            if self.facing_left:  # Si le sprite regarde vers la gauche, rétablir l'image normale
+            if self.facing_left:
                 self.image = pygame.transform.flip(self.image, True, False)
                 self.facing_left = False
-            direction[0] = vitesse  # Déplacer le sprite vers la droite
+            direction[0] = vitesse
 
         if touches[pygame.K_UP]:
-            direction[1] = -vitesse  # Déplacer le sprite vers le haut
+            direction[1] = -vitesse
         elif touches[pygame.K_DOWN]:
-            direction[1] = vitesse  # Déplacer le sprite vers le bas
+            direction[1] = vitesse
 
-            # Mettre à jour la position du sprite en fonction de la direction
+        # Mettre à jour la position du sprite et de la hitbox
         self.last_pos = (self.rect.x, self.rect.y)
         self.rect.x += direction[0]
         self.rect.y += direction[1]
+        self.hitbox.x = self.rect.x
+        self.hitbox.bottom = self.rect.bottom  # Gardez la hitbox alignée avec les pieds
 
     def checkCollision(self, carte, type):
         liste_collision = []
-
         for object in carte.objects:
             if object.type == type:
                 rect = pygame.Rect(object.x, object.y, object.width, object.height)
                 liste_collision.append(rect)
-        if self.rect.collidelist(liste_collision) > -1:
+        # Vérifier la collision avec la hitbox
+        if self.hitbox.collidelist(liste_collision) > -1:
             return True
         else:
             return False
